9623d007a7c5a198c02db07c40decf9ce42953a2
Gecko Uno <dominica.patterson@gmail.com>
2020-12-04 16:56:35 -0800

    Finish Emoji Project

---
+++ src/emojipedia.js
@@ -0,0 +1,25 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  }
+];
+
+export default emojipedia;
